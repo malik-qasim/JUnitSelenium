a31ed03430d4029e3159fd15549b3eb11b6c81d4
malik-qasim <66017328+malik-qasim@users.noreply.github.com>
2025-05-13 09:47:47 +0500

    Update JUnitSeleniumTest.java

--- src/test/java/JUnitSeleniumTest.java
+++ src/test/java/JUnitSeleniumTest.java
@@ -14,7 +14,7 @@ class JUnitSeleniumTest {
 
 	@Test
 	void test_login_with_incorrect_credentials() {
-		System.setProperty("webdriver.chrome.driver", "/home/ubuntu/chromedriver-linux64/");
+		System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
 		ChromeOptions options = new ChromeOptions();
         	options.addArguments("--headless");               // Run in headless mode
         	options.addArguments("--no-sandbox");             // For Ubuntu
